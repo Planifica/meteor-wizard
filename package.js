6914c7171f4b026e956e7aad522810908c50954f
PhilippSpo <philipp.sporrer@planifica.de>
2015-01-28 21:03:08 +0100

    version bump

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'planifica:wizard',
   summary: "A wizard component for AutoForm.",
-  version: '0.0.3',
+  version: '0.0.4',
   git: 'https://github.com/Planifica/meteor-wizard.git'
 });
 
